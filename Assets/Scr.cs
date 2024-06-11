a72ee62be9a0ce577afe0a47b94bea233f95aa1b
Alino22Git <138936464+Alino22Git@users.noreply.github.com>
2024-06-11 21:10:35 +0200

    Imported Assets+ Edited Script

--- Assets/Scr.cs
+++ Assets/Scr.cs
@@ -5,24 +5,28 @@ using UnityEngine.UI;
 
 public class Scr : MonoBehaviour
 {
-    Image timerBar;
     public float maxTime = 5f;
     public float timeLeft;
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+    public float speed = 0.0f;
     void Start()
     {
-        timerBar = GetComponent<Image>();
-        timeLeft = maxTime;
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = maxTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!timerBar) return;
-        
-        if (timeLeft > 0)
+        if (!imageComp) return;
+
+        if (imageComp.fillAmount != 0f)
         {
-            timeLeft -= Time.deltaTime;
-            timerBar.fillAmount = timeLeft / maxTime;
+            imageComp.fillAmount = imageComp.fillAmount - Time.deltaTime * speed;
+
         }
         else
         {
