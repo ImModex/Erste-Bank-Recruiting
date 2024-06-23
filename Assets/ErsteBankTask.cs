4f93797ceca84d731dabf7057bbccd2a5344c339
Michael Reichenberger <michael.reichenberger@gmx.at>
2024-06-23 19:38:34 +0200

    Project Cleanup

---
+++ Assets/ErsteBankTask.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpatialSys.Samples.InputOverride
+{
+    public class ErsteBankTask : MonoBehaviour
+    {
+
+        public GameObject loadingBar;
+       
+        public void startTask()
+        {
+          
+        }
+        
+    }
+}
--- Assets/loadingBar/scripts/loadingbar.cs
+++ Assets/loadingBar/scripts/loadingbar.cs
@@ -7,54 +7,35 @@ public class loadingbar : MonoBehaviour
 {
     private RectTransform rectComponent;
     private Image imageComp;
-    public float speed = 0.001f;
+    public float maxTime = 50.0f;
+    private float timeLeft;
     public Image borderImage;
     public Text textComp;
     public GameObject teleporter;
-    // Use this for initialization
     void Start()
     {
         rectComponent = GetComponent<RectTransform>();
         imageComp = GetComponent<Image>();
-        if (imageComp != null)
-        {
-            imageComp.fillAmount = 1.0f;
-        }
-        else
-        {
-            Debug.LogError("Image component is missing on this GameObject.");
-        }
-
-        // Annahme: Das borderImage ist das Image des Elternobjekts "loading3"
+        timeLeft = maxTime;
+        imageComp.fillAmount = 1.0f;
         borderImage = transform.parent.GetComponent<Image>();
-        if (borderImage == null)
-        {
-            Debug.LogError("Border image not found in parent GameObject.");
-        }
-
-        // Finde das Text-Element im Elternobjekt
         textComp = transform.parent.GetComponentInChildren<Text>();
-        if (textComp == null)
-        {
-            Debug.LogError("Text component not found in parent GameObject.");
-        }
     }
 
     void Update()
     {
         if (imageComp != null && imageComp.fillAmount > 0.0f)
         {
-            imageComp.fillAmount -= Time.deltaTime * speed;
+            timeLeft -= Time.deltaTime;
+            imageComp.fillAmount = timeLeft / maxTime;
         }
         else if (imageComp != null && imageComp.fillAmount <= 0.0f)
         {
-            // Blende das borderImage aus
+
             if (borderImage != null)
             {
                 borderImage.enabled = false;
             }
-
-            // Blende das Text-Element aus
             if (textComp != null)
             {
                 textComp.enabled = false;
@@ -64,7 +45,7 @@ public class loadingbar : MonoBehaviour
 
             // Szene neu laden
             //CustomSceneManager.Restart();
-            teleporter.SetActive(true);
+            //teleporter.SetActive(true);
         }
     }
 }
