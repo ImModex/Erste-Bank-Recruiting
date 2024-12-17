0e683f518cadfb093c6847c1b95ba59a3bca1ad6
JohnnyJoster3223 <joreiter4@gmail.com>
2024-12-17 19:05:54 +0100

    NPCs Added for better Immersion Also Ineractable fix

---
+++ Assets/Erste Bank/Lookatplayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpatialSys.Samples.InputOverride
+{
+    public class Lookatplayer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
